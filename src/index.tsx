1ce2c39cada3c999b58a67289e428ef96b0a871a
Mustafa <33766083+MkMan@users.noreply.github.com>
2023-01-10 18:28:34 +1100

    chore: initial setup
    Remove SASS & Stylelint
    Add Mantine

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
-import './index.scss';
+import './index.css';
 
+import { MantineProvider, Title } from '@mantine/core';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
-import App from './App';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- defined in index.html
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <Title>Pomodoro</Title>
+    </MantineProvider>
   </React.StrictMode>
 );
