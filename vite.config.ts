cf2865f809d50df58e06e8264b61fc2d4c59de91
Mustafa <33766083+MkMan@users.noreply.github.com>
2023-01-13 20:10:56 +1100

    chore: set up ts paths for utils folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
