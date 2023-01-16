41de2343b447c536cc35b4817bca94ed6ca9fe1d
Mustafa <33766083+MkMan@users.noreply.github.com>
2023-01-16 20:03:39 +1100

    refactor: move index.tsx file outside src

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { App } from './app/app';
+import { App } from './src/app';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- defined in index.html
 createRoot(document.getElementById('root')!).render(
