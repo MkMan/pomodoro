5870facdcfe89274ff30f56a99533fc59c9d4c3d
Mustafa <33766083+MkMan@users.noreply.github.com>
2023-03-04 00:36:15 +1100

    refactor(settings): move durations out to a new comp

---
+++ src/app/settings/durations/durations.tsx
@@ -0,0 +1,48 @@
+import { Component, ComponentProps, For } from 'solid-js';
+import { AppSettings } from 'src/state/settings/types';
+
+import { Heading, Input } from '$app-components';
+import { counterState, setSettingsStore, settingsStore } from '$app-state';
+
+type DurationField = {
+  label: string;
+  stateName: keyof AppSettings['durations'];
+};
+
+const durations = settingsStore.durations;
+
+const commonInputProps: Partial<ComponentProps<typeof Input>> = {
+  isRequired: true,
+  mb: 16,
+  type: 'number',
+  min: 1,
+};
+
+const durationFields: DurationField[] = [
+  { label: 'Pomodoro', stateName: 'pomodoro' },
+  { label: 'Short break', stateName: 'shortBreak' },
+  { label: 'Long break', stateName: 'longBreak' },
+];
+
+export const Durations: Component = () => (
+  <section>
+    <Heading level={3} mb={16}>
+      Durations
+    </Heading>
+    <For each={durationFields}>
+      {({ stateName, label }) => (
+        <Input
+          {...commonInputProps}
+          disabled={counterState() !== 'stopped'}
+          error={durations[stateName] <= 0 && `${label} must be greater than 0`}
+          label={label}
+          onInput={(event) => {
+            const value = event.currentTarget.valueAsNumber || 0;
+            setSettingsStore('durations', stateName, value);
+          }}
+          value={durations[stateName] === 0 ? '' : durations[stateName]}
+        />
+      )}
+    </For>
+  </section>
+);
--- src/app/settings/settings.tsx
+++ src/app/settings/settings.tsx
@@ -1,56 +1,15 @@
-import { Component, ComponentProps, For } from 'solid-js';
-import { AppSettings } from 'src/state/settings/types';
+import { Component } from 'solid-js';
 
-import { Heading, Input } from '$app-components';
-import { counterState, setSettingsStore, settingsStore } from '$app-state';
+import { Durations } from './durations/durations';
 
 type Props = {
   dataTestId?: string;
 };
 
-type DurationField = {
-  label: string;
-  stateName: keyof AppSettings['durations'];
-};
-
 export const Settings: Component<Props> = (props) => {
-  const durations = settingsStore.durations;
-
-  const commonInputProps: Partial<ComponentProps<typeof Input>> = {
-    isRequired: true,
-    mb: 16,
-    type: 'number',
-    min: 1,
-  };
-
-  const durationFields: DurationField[] = [
-    { label: 'Pomodoro', stateName: 'pomodoro' },
-    { label: 'Short break', stateName: 'shortBreak' },
-    { label: 'Long break', stateName: 'longBreak' },
-  ];
-
   return (
-    <section data-testid={props.dataTestId}>
-      <Heading level={3} mb={16}>
-        Durations
-      </Heading>
-      <For each={durationFields}>
-        {({ stateName, label }) => (
-          <Input
-            {...commonInputProps}
-            disabled={counterState() !== 'stopped'}
-            error={
-              durations[stateName] <= 0 && `${label} must be greater than 0`
-            }
-            label={label}
-            onInput={(event) => {
-              const value = event.currentTarget.valueAsNumber || 0;
-              setSettingsStore('durations', stateName, value);
-            }}
-            value={durations[stateName] === 0 ? '' : durations[stateName]}
-          />
-        )}
-      </For>
-    </section>
+    <div data-testid={props.dataTestId}>
+      <Durations />
+    </div>
   );
 };
