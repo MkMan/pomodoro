6feb9100553f8d66a008f3d78a83f1c4b8f9dbe0
Mustafa <33766083+MkMan@users.noreply.github.com>
2023-04-09 12:26:18 +1000

    fix(app): fix access bug in `Select`

--- src/components/select/types.ts
+++ src/components/select/types.ts
@@ -1,7 +1,9 @@
 import { JSX } from 'solid-js';
 
-export type SelectProps = {
+type SelectProps = {
   value: string;
   onChange: JSX.EventHandler<HTMLSelectElement, Event>;
   options: { label: string; value: string }[];
 } & Omit<JSX.SelectHTMLAttributes<HTMLSelectElement>, 'onChange' | 'value'>;
+
+export type { SelectProps };
