201bf99870d99f9820695944befd59188f947caf
Mustafa <33766083+MkMan@users.noreply.github.com>
2023-05-14 07:29:20 +1000

    feat(todos): improve new todo form

--- src/app/todos/new-todo/new-todo.tsx
+++ src/app/todos/new-todo/new-todo.tsx
@@ -1,9 +1,8 @@
-import { FiPlus } from 'solid-icons/fi';
-import { Component, createSignal } from 'solid-js';
+import { FiX } from 'solid-icons/fi';
+import { Component, createEffect, createSignal } from 'solid-js';
 
-import { IconButton, Input, Select } from '$app-components';
+import { Button, Heading, IconButton, Input, Select } from '$app-components';
 import { TodoFrequency } from '$app-state';
-import { cx } from '$app-utils';
 
 import * as styles from './styles';
 import { NewTodoProps } from './types';
@@ -12,6 +11,8 @@ const initialDescription = '';
 const initialFrequency: TodoFrequency = 'once';
 
 const NewTodo: Component<NewTodoProps> = (props) => {
+  let descriptionRef: HTMLInputElement | undefined;
+
   const [description, setDescription] = createSignal(initialDescription);
   const [frequency, setFrequency] =
     createSignal<TodoFrequency>(initialFrequency);
@@ -31,29 +32,50 @@ const NewTodo: Component<NewTodoProps> = (props) => {
     setFrequency(initialFrequency);
   };
 
+  createEffect(() => {
+    descriptionRef?.focus();
+  });
+
   return (
-    <form class={cx(props.class, styles.wrapper)} onSubmit={onSubmit}>
-      <Input
-        class={styles.description}
-        placeholder="New todo description"
-        title="New todo description"
-        value={description()}
-        onInput={({ currentTarget }) => setDescription(currentTarget.value)}
-      />
-      <Select
-        title="Frequency"
-        options={[
-          { label: 'Once', value: initialFrequency },
-          { label: 'Daily', value: 'daily' },
-        ]}
-        value={frequency()}
-        onChange={({ currentTarget }) =>
-          setFrequency(currentTarget.value as TodoFrequency)
-        }
-      />
-      <IconButton type="submit" title="Create">
-        <FiPlus size={30} />
-      </IconButton>
+    <form name="new todo details" class={props.class} onSubmit={onSubmit}>
+      <div class={styles.headingWrapper}>
+        <Heading class={styles.heading} level={3}>
+          New todo
+        </Heading>
+        <IconButton
+          type="button"
+          onClick={props.onClose}
+          title="close new todo form"
+        >
+          <FiX size={25} />
+        </IconButton>
+      </div>
+      <div class={styles.inputsWrapper}>
+        <Input
+          label="New todo description"
+          value={description()}
+          onInput={({ currentTarget }) => setDescription(currentTarget.value)}
+          ref={descriptionRef}
+        />
+        <div class={styles.frequencyWrapper}>
+          <label for="new-todo-frequency">Frequency</label>
+          <Select
+            class={styles.frequency}
+            id="new-todo-frequency"
+            options={[
+              { label: 'Once', value: initialFrequency },
+              { label: 'Daily', value: 'daily' },
+            ]}
+            value={frequency()}
+            onChange={({ currentTarget }) =>
+              setFrequency(currentTarget.value as TodoFrequency)
+            }
+          />
+        </div>
+      </div>
+      <Button class={styles.createCta} size="small" type="submit">
+        Create
+      </Button>
     </form>
   );
 };
