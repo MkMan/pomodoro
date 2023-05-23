e6735fea16ae5987d50040f495d650e75b985b73
Mustafa <33766083+MkMan@users.noreply.github.com>
2023-05-24 07:22:48 +1000

    fix(reload-prompt): correct colours

--- src/app/reload-prompt/styles.ts
+++ src/app/reload-prompt/styles.ts
@@ -14,17 +14,19 @@ const toast = css({
   margin: 16,
   padding: 16,
   border: '1px solid #8885',
-  borderRadius: 4,
+  borderRadius: 8,
   zIndex: 1,
   textAlign: 'left',
   boxShadow: '3px 4px 5px 0 #8885',
-  backgroundColor: 'white',
+  backgroundColor: 'var(--color-background-inverted)',
 });
 
 const toastMessage = css({
   marginBottom: 16,
   fontSize: '1.2rem',
   fontWeight: 'bold',
+  color: 'var(--color-font-inverted)',
+  paddingBlockEnd: 16,
 });
 
 const buttonsWrapper = css({
