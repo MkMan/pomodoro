7063ce14d849fbe0c0a161f43c09a8d25d899141
Mustafa <33766083+MkMan@users.noreply.github.com>
2023-05-25 19:28:54 +1000

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import solidPlugin from 'vite-plugin-solid';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     solidPlugin(),
