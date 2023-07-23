8841e48590b65a8885ca5a5db5c81a2b429a8d12
Mustafa <33766083+MkMan@users.noreply.github.com>
2023-07-23 14:19:02 +1000

    chore: migrate Header to vanilla extract

--- src/app/header/styles.ts
+++
@@ -1,9 +0,0 @@
-import { css } from '@emotion/css';
-
-export const headerContentWrapper = css({
-  display: 'flex',
-  columnGap: 10,
-  alignItems: 'center',
-});
-
-export const header = css({ borderBottom: '1px solid lightgrey' });
