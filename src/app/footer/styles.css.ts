9e97bff5224d71a307656ad85200f69bfda87d3f
Mustafa <33766083+MkMan@users.noreply.github.com>
2023-07-23 14:34:50 +1000

    chore: migrate Footer to vanilla extract

--- src/app/footer/styles.css.ts
+++ src/app/footer/styles.css.ts
@@ -1,15 +1,15 @@
-import { css } from '@emotion/css';
+import { style } from '@vanilla-extract/css';
 
-export const footer = css({
+export const footer = style({
   borderBlockStart: '1px solid lightgrey',
   paddingBlock: 16,
 });
 
-export const wrapper = css({
+export const wrapper = style({
   display: 'flex',
   alignItems: 'center',
 });
 
-export const sourceCodeLink = css({
+export const sourceCodeLink = style({
   marginInlineStart: 'auto',
 });
