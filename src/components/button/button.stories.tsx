432193908672a7c2fd109612c69f3702344d7275
Mustafa <33766083+MkMan@users.noreply.github.com>
2023-09-09 19:59:30 +1000

    chore: add some stories

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from 'storybook-solidjs';
+
+import { Button } from './button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/solid/writing-stories/introduction
+const meta = {
+  component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/solid/writing-stories/args
+export const Pallet: Story = {
+  render: () => (
+    <div
+      style={{
+        display: 'grid',
+        'justify-content': 'center',
+        gap: '24px',
+        'grid-template-columns': 'repeat(3, 1fr)',
+      }}
+    >
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="danger">Danger</Button>
+      <Button size="small">Primary</Button>
+      <Button size="small" variant="secondary">
+        Secondary
+      </Button>
+      <Button size="small" variant="danger">
+        Danger
+      </Button>
+    </div>
+  ),
+};
---
+++ src/components/collapse/collapse.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from 'storybook-solidjs';
+
+import { Collapse } from './collapse';
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/solid/writing-stories/introduction
+const meta = {
+  component: Collapse,
+} satisfies Meta<typeof Collapse>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: 'Click to expand/collapse',
+    headingLevel: 2,
+    content: (
+      <>
+        <h3>The tragedy of darth Plagueis the wise</h3>
+        <br />
+        <p>
+          Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he
+          could use the Force to influence the midichlorians to create life… He
+          had such a knowledge of the dark side that he could even keep the ones
+          he cared about from dying.
+        </p>
+        <br />
+        <p>
+          The dark side of the Force is a pathway to many abilities some
+          consider to be unnatural. He became so powerful… the only thing he was
+          afraid of was losing his power, which eventually, of course, he did.
+          Unfortunately, he taught his apprentice everything he knew, then his
+          apprentice killed him in his sleep. Ironic. He could save others from
+          death, but not himself.
+        </p>
+      </>
+    ),
+  },
+};
