3cf7cd5e12b84c38c911ba9db1a553e6d208ed78
Mustafa <33766083+MkMan@users.noreply.github.com>
2023-09-17 07:32:19 +1000

    chore: add theme switcher to Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,43 @@
-import { Preview } from 'storybook-solidjs';
+import { Decorator, Preview } from 'storybook-solidjs';
 
 import '../src/css/dimensions.css';
 import '../src/css/fonts.css';
 import '../src/css/reset.css';
 
-import { lightThemeClassName } from '../src/theme/index.css';
+import {
+  lightThemeClassName,
+  darkThemeClassName,
+} from '../src/theme/index.css';
+import { createEffect } from 'solid-js';
 
-globalThis.document.body.classList.add(lightThemeClassName);
+const withTheme: Decorator = (StoryFn, context) => {
+  const theme = context.parameters.theme || context.globals.theme;
+
+  createEffect(() => {
+    globalThis.document.body.setAttribute(
+      'class',
+      theme === 'dark' ? darkThemeClassName : lightThemeClassName,
+    );
+  });
+
+  return StoryFn();
+};
+
+const globalTypes = {
+  theme: {
+    name: 'theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: [
+        { value: 'light', icon: 'circlehollow', title: 'light' },
+        { value: 'dark', icon: 'circle', title: 'dark' },
+      ],
+      showName: true,
+    },
+  },
+};
 
 const preview: Preview = {
   parameters: {
@@ -18,6 +49,9 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [withTheme],
 };
 
 export default preview;
+
+export { globalTypes };
