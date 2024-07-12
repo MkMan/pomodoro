a18f0361c90aebc796f4d7ad8215633a65594ab5
Mustafa Mansour <33766083+MkMan@users.noreply.github.com>
2024-07-12 19:11:19 +1000

    chore: remove pnpm

--- src/app/todos/todo-item/types.ts
+++ src/app/todos/todo-item/types.ts
@@ -1,12 +1,13 @@
 import { Todo } from '$app-state';
 import { JSX } from 'solid-js';
 
-type TodoItemProps = Todo & {
+type TodoItemProps = {
   class?: string;
   onDelete: () => void;
   onDescriptionChange: (description: Todo['description']) => void;
   onStatusChange: (status: Todo['status']) => void;
-} & JSX.HTMLAttributes<HTMLLIElement>;
+} & JSX.HTMLAttributes<HTMLLIElement> &
+  Todo;
 
 type Mode = 'display' | 'edit';
 
